cced01152cae52e200e28b8a9fcc6bc4fcaf2046
bengjerstad <bengjerstad@gmail.com>
2016-09-11 21:11:26 -0400

    2to3
    Used 2to3 to convert the files. Still have an encoding error that needs
    to be fixed.

--- example.py
+++ example.py
@@ -10,23 +10,23 @@ Rules for parsing:
     Chapters are surrounded by # (ex #3#)
     Paragraph Title are surrounded by * (ex. *For God So Loved the World*)
 """
-print "****** Result for passage John 3:1-15 ********"
-print "Reference :"+result['reference']
-print "Version :"+result['version']
-print result['text'].replace('*', '\n\n').replace('#', '\n')
+print("****** Result for passage John 3:1-15 ********")
+print("Reference :"+result['reference'])
+print("Version :"+result['version'])
+print(result['text'].replace('*', '\n\n').replace('#', '\n'))
 
 #search result in bible getting a dictionary (reference,text)
 results = bg_api.get_search_result("Fruit Spirit")
 
-print "\n\n****** Result for searching Fruit Spirit ********"
-for (reference, text) in results.items():
-    print "Reference: "+reference
-    print "Text: "+text
+print("\n\n****** Result for searching Fruit Spirit ********")
+for (reference, text) in list(results.items()):
+    print("Reference: "+reference)
+    print("Text: "+text)
 
 
 #get verse of the day
 result = bg_api.getVotd()
-print "\n\n****** Verse of the Day ********"
-print "Reference :"+result['reference']
-print "Version :"+result['version']
-print result['text']
+print("\n\n****** Verse of the Day ********")
+print("Reference :"+result['reference'])
+print("Version :"+result['version'])
+print(result['text'])
